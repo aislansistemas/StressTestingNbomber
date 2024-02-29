a5331b34aac10e40ad4039c8a571857dbf25e156
aislan <bliza@LAPTOP-C8IK964T>
2024-02-29 11:29:42 -0300

    Cria primeiro teste de carga

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NBomber.Contracts.Stats;
+using NBomber.CSharp;
+
+using var httpClient = new HttpClient();
+
+var scenario = Scenario.Create("Primeiro teste de carga", async context => {
+    var response = await httpClient.GetAsync("https://google.com.br");
+
+    return response.IsSuccessStatusCode
+        ? Response.Ok()
+        : Response.Fail();
+})
+.WithoutWarmUp() //removendo aquecimento
+.WithLoadSimulations(
+    Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(1), during: TimeSpan.FromSeconds(30))
+); // define carga de teste
+
+NBomberRunner.RegisterScenarios(scenario).Run();
